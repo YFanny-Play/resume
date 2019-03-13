22e3e0875b68b4b9f061f5cfe37c3bb08385e808
Xuyaofang <1106862002@qq.com>
2019-03-13 23:33:27 +0800

    添加koa-router

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const Koa = require('koa');
-const app = new Koa();
+const Router = require('koa-router');
+const Fs = require('fs');
+const Path = require('path');
 
-app.use(async ctx => {
-    ctx.body = 'hello world';
+const app = new Koa();
+const router = new Router();
+
+// app.use(async ctx => {
+//     ctx.body = 'hello world';
+// })
+router.get('/', async (ctx, next) => {
+    ctx.type = 'text/html';
+    ctx.body = Fs.createReadStream(
+        Path.resolve(__dirname, './www/index.html')
+    );
 })
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(3000);
