513b88a588724b2daf6f36def00a898ba525e42e
Xuyaofang <1106862002@qq.com>
2019-03-14 00:13:53 +0800

    koa-static

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const Koa = require('koa');
+const static = require('koa-static');
 const Router = require('koa-router');
 const Fs = require('fs');
-const Path = require('path');
+const path = require('path');
 
 const app = new Koa();
 const router = new Router();
+
+// 静态资源目录对于相对入口文件index.js的路径
+const staticPath = './static'
+app.use(static(
+    path.join(__dirname, staticPath)
+))
 
 // app.use(async ctx => {
 //     ctx.body = 'hello world';
@@ -12,7 +19,7 @@ const router = new Router();
 router.get('/', async (ctx, next) => {
     ctx.type = 'text/html';
     ctx.body = Fs.createReadStream(
-        Path.resolve(__dirname, './www/index.html')
+        path.resolve(__dirname, './www/index.html')
     );
 })
 
